42d8d8ba4600d5ef818eeb86da9c9f217c7e4f3e
samaralnasib <148685912+samaralnasib@users.noreply.github.com>
2023-10-26 15:01:25 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Q1
+        //int firstnum = 125;
+        //int secondnum = 24;
+        //System.out.println("Ruselt");
+        //int a = firstnum + secondnum;
+        //int b = firstnum - secondnum;
+        //int c = firstnum * secondnum;
+        //int d = firstnum / secondnum;
+        //System.out.println(a);
+        //System.out.println(b);
+        //System.out.println(c);
+        //System.out.println(d);
+
+        //Q2
+        //Scanner input = new Scanner(System.in);
+        //System.out.println("Enter the number:");
+        //int num = input.nextInt();
+
+        //int i =1;
+        //System.out.println("X" + num + ":");
+        //do { int multip = num * i;
+        //    System.out.println(num + " x " + i + " = " + multip);
+        //    i=i+1;
+        //}
+        //while(i <= 10);
+
+
+        //Q6
+        //Scanner input = new Scanner(System.in);
+        //System.out.println("Enter the word ");
+        //String word = input.nextLine();
+        //StringBuilde helloword = new StringBuilde(word);
+        //helloword.reverse();
+        //System.out.println("Reversed: " + helloword);
+
+
+
+        //Q11
+        //Scanner input = new Scanner(System.in);
+
+        //System.out.println("Enter the first number: ");
+        //int firstnum = input.nextInt();
+        //System.out.println("Enter the second number: ");
+        //int secondnum = input.nextInt();
+
+        //if (firstnum == secondnum) {
+        //    System.out.println("not equal");
+        //} else if (firstnum > secondnum) {
+        //   System.out.println("The num1 is greater than num2");
+        //} else if (firstnum >= secondnum) {
+        //  System.out.println("The num1 is greater or equal than num2");
+        //} else System.out.println("The num2 is greater than num1");
+
+        //Q7
+        //Scanner input = new Scanner(System.in);
+        //System.out.println("Enter a number: ");
+        //int num = input.nextInt();
+        //if (num % 2 == 0) {
+            //System.out.println(num + "even");
+        //} else System.out.println(num + "odd");
+
+        //Q 13
+        //Scanner input = new Scanner(System.in);
+        //System.out.println("enter the first number");
+        //int num1 = input.nextInt();
+        //System.out.println("enter the second number");
+        //int num2 = input.nextInt();
+        //System.out.println("enter the third number");
+        //int num3 = input.nextInt();
+        //System.out.println("enter the forth number");
+        //int num4 = input.nextInt();
+
+        //if(num1==num2){
+            //System.out.println("equal");
+        //} else if (num1==num3) {
+            //System.out.println("equal");
+
+        //} else if (num1==num4) {
+          //  System.out.println("equal");
+
+        //}else System.out.println("Numbers not equal");
+
+        //Q14
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the number");
+        int num = input.nextInt();
+        if (num>0);{
+            System.out.println("positive");
+        } else if (num<0);{
+            System.out.println("negative");
+        } else System.out.println("zero");
+
+    }
+}
+
+
+
+
+
+
